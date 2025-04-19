06ddef2dcbc923be9c8994d95af92839309d9492
Harsh Patil <itsharshhh6@gmail.com>
2025-04-19 17:36:13 +0530

    git commit

--- src/main/java/com/embarkx/FirstSpring/HelloWorld.java
+++ src/main/java/com/embarkx/FirstSpring/HelloWorld.java
@@ -1,6 +1,8 @@
 package com.embarkx.FirstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloWorld {
     @GetMapping("/greetings")
     public String hello(){
         return "Hiii";
+    }
+
+    @PostMapping("/namehello")
+    public String namehello(@RequestBody String name){
+        return "Hiii " + name + "!";
     }
 }
